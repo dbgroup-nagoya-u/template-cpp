3404dda345dd82fefad90e441f5bed2e6c7eb406
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-05-01 14:33:21 +0900

    remove glog and add gflags

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 #include "gflags/gflags.h"
-#include "glog/logging.h"
 
 // set command line options
 DEFINE_bool(test, false, "test");
@@ -10,22 +9,8 @@ DEFINE_bool(test, false, "test");
 int
 main(int argc, char* argv[])
 {
-  // init glog with a program name
-  google::InitGoogleLogging(argv[0]);
-
   // parse and remove command line options
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-
-  // `LOG` macro always outputs messages
-  LOG(INFO) << "Hello, world!";
-
-  // `DLOG` macro will be excluded in Release build
-  DLOG(INFO) << "Hello, debug world!";
-
-  // set options can be accessed with `FLAGS_` prefix
-  if (FLAGS_test) {
-    LOG(INFO) << "This message is shown only if '--test' option is specified.";
-  }
 
   return 0;
 }
