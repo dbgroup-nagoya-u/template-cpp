e40091989525b76d0722518c1910c404922af47f
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-12-05 10:37:58 +0900

    enable some rules of clang-tidy

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -40,9 +40,9 @@ ValidateNonZero(  //
  * CLI arguments
  *################################################################################################*/
 
-DEFINE_bool(show_value, true, "Show a value to stdout if true");
-DEFINE_uint64(nonzero_value, 1, "An option to set a positive value");
-DEFINE_validator(nonzero_value, &ValidateNonZero);
+DEFINE_bool(show_value, true, "Show a value to stdout if true");       // NOLINT
+DEFINE_uint64(nonzero_value, 1, "An option to set a positive value");  // NOLINT
+DEFINE_validator(nonzero_value, &ValidateNonZero);                     // NOLINT
 
 /*##################################################################################################
  * Main function
